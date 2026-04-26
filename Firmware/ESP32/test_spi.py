d5d57d49768a56381699686c04299a5162eb8a2d
roc-ket-cod-er <gargm0325@wrdsb.ca>
2026-04-26 14:02:43 -0400

    Create test_spi.py

---
+++ Firmware/ESP32/test_spi.py
@@ -0,0 +1,40 @@
+from machine import SPI, Pin
+from time import sleep_us, ticks_ms
+
+spi = SPI(
+    1,
+    baudrate=1000000,   # start slow for testing
+    polarity=0,        # must match STM32 CPOL Low
+    phase=0,           # must match STM32 CPHA 1 Edge
+    bits=8,
+    firstbit=SPI.MSB,
+    sck=Pin(9),
+    mosi=Pin(18),
+    miso=Pin(8)
+)
+
+
+def read_bytes():
+    tx_dummy = bytes([255]*5)   # send dummy byte (0xFF)
+    rx = bytearray(5)
+
+    spi.write_readinto(tx_dummy, rx)
+    return list(rx)
+
+lv = []
+
+while True:
+    value = read_bytes()
+
+    if value[2] == 0 and (value[0] == 255 or value[0] == 0) and value[4] < 2 and value[1] != 0 and value[3] != 0:
+        if value != lv:
+            print("Raw byte:", value, f"    \tx: {value[1]:03d} y: {value[3] + 256*value[4]:03d} tstamp: {ticks_ms()%10000}")
+            lv = value
+    elif value == [0,0,0,0,0]:
+        pass
+        #print(value)
+    else:
+        pass
+        #print(value)
+    
+    sleep_us(1000)
