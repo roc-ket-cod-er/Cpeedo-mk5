6c19d8e88a933df5b494676c7836d8dca9aed88b
roc-ket-cod-er <gargm0325@wrdsb.ca>
2026-05-01 16:08:46 -0400

    Create race.py

---
+++ Firmware/ESP32/race.py
@@ -0,0 +1,48 @@
+# Important Imports
+from machine import Pin, UART, freq, ADC
+import uasyncio as asyncio
+from random import randint
+
+# Prevent Automatic Shutdown / Comms
+stm_com = Pin(43, Pin.OUT)
+stm_com.off()
+
+# Imports
+from sim7080g import Cell
+from gps import GPS
+from time import sleep_ms, ticks_ms
+
+cell = Cell()
+gps = GPS()
+
+spd = 20
+
+adc = ADC(Pin(3))
+adc.atten(ADC.ATTN_11DB)
+adc.width(ADC.WIDTH_12BIT)
+
+while True:
+    loop_start=ticks_ms()
+    gps.ban_updates()
+    
+    cell.connect('io')
+    
+    spd += randint(-30, 30)/10
+    spd = min(40, spd)
+    spd = max(0, spd)
+    spd = gps.spd
+    
+    raw = adc.read()
+    voltage_adc = raw / 4095 * 3.3
+    battery_voltage = round(voltage_adc * (127 / 27), 3)
+
+    print(battery_voltage)
+    
+    if gps.lock:
+        cell.send_message(f'{battery_voltage}V {spd}', "ev-run")
+    else:
+        cell.send_message(f'{battery_voltage}V-nolock {spd}', "ev-run")
+    print(2100+loop_start-ticks_ms())
+    gps.allow_updates()
+    while 2100+loop_start-ticks_ms() > 0:
+        gps.update()
